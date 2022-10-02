95362ac9fe4dea7857275aefaab84bda6c2803c1
aleksey <glinkirk@gmail.com>
2022-10-02 19:21:07 +0300

    счётчик готов

--- js/script.js
+++ js/script.js
@@ -20,19 +20,22 @@ window.addEventListener('click', function (event) {
 
 
     let counter;
-
+    let finalItem = document.querySelector('#final__item');
 //////////////////////////
     if (event.target.dataset.action === 'plusAll' || event.target.dataset.action === 'minusAll') {
 
         const counterWrapper = event.target.closest('.button__calc');
 
         counter = counterWrapper.querySelector('[data-counter]');
+
     }
 
 
 //////////////////////////
     if (event.target.dataset.action === 'plusAll') {
 
+        finalItem.innerText = ++finalItem.innerText;
+        calcCartPrice();
         counter.innerText = ++counter.innerText;
     }
 
@@ -41,7 +44,36 @@ window.addEventListener('click', function (event) {
     if (event.target.dataset.action === 'minusAll') {
 
         if(parseInt(counter.innerText) > 0) {
+            finalItem.innerText = --finalItem.innerText;
             counter.innerText = --counter.innerText;
+
         } 
     }
+
+    if (event.target.hasAttribute('data-action')) {
+        calcCartPrice();
+    }
 });
+
+
+
+function calcCartPrice() {
+
+    const mainItem =  document.querySelectorAll('.item');
+
+    let finalPrice = document.querySelector('.final__price');
+
+    let totalPrice = 0;
+
+    mainItem.forEach(function (item) {
+
+        const amountEl = item.querySelector('[data-counter]');
+        const priceEl = item.querySelector('.item__price');
+
+        const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
+        totalPrice = totalPrice + currentPrice;
+
+    });
+
+    finalPrice.innerText = totalPrice;
+}
